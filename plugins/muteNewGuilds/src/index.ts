3975027a48e8151cf3fd1682be32e83f11ebf19f
Beef <beefers@riseup.net>
2023-04-22 15:48:23 +0000

    muteNewGuilds: Fix unpatching

--- plugins/muteNewGuilds/src/index.ts
+++ plugins/muteNewGuilds/src/index.ts
@@ -11,27 +11,25 @@ storage.server ??= false;
 storage.everyone ??= true;
 storage.roles ??= true;
 
-async function patch() {
-    const [isMuted, isEveryoneSupressed, isRolesSupressed] = [await UserGuildSettingsStore.isMuted(null), await UserGuildSettingsStore.isSuppressEveryoneEnabled(null), await UserGuildSettingsStore.isSuppressRolesEnabled(null)];
-    if (isMuted || isEveryoneSupressed || isRolesSupressed) {
-        updateGuildNotificationSettings(null, { "muted": false, "suppress_everyone": false, "suppress_roles": false });
-    }
-
-    return after("acceptInvite", invites, async (args, res) => {
-        const guildId = await res.then(x => {
-            return x.guild.id;
-        });
-        if (guildId === "@me" || guildId === "null" || guildId == null) return;
-        updateGuildNotificationSettings(guildId, { "muted": storage.server, "suppress_everyone": storage.everyone, "suppress_roles": storage.roles });
-    });
-}
+let unpatch;
 
 export default {
     onLoad: () => {
-        patch();
+        const [isMuted, isEveryoneSupressed, isRolesSupressed] = [await UserGuildSettingsStore.isMuted(null), await UserGuildSettingsStore.isSuppressEveryoneEnabled(null), await UserGuildSettingsStore.isSuppressRolesEnabled(null)];
+        if (isMuted || isEveryoneSupressed || isRolesSupressed) {
+            updateGuildNotificationSettings(null, { "muted": false, "suppress_everyone": false, "suppress_roles": false });
+        }
+        
+        // `after` (and the other patcher functions) return a function that removes the first patch
+        unpatch = after("acceptInvite", invites, async (args, res) => {
+            const guildId = await res.then(x => {
+                return x.guild.id;
+            });
+            if (guildId === "@me" || guildId === "null" || guildId == null) return;
+            updateGuildNotificationSettings(guildId, { "muted": storage.server, "suppress_everyone": storage.everyone, "suppress_roles": storage.roles });
+        });
     },
-    onUnload: () => {
-        patch();
-    },
+    // `unpatch` is a function here so we can just directly pass it to onUnload
+    onUnload: unpatch,
     settings: Settings
 };
