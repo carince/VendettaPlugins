db7d2a356900c2ab5990b10341a9cf92b2fdd151
Norikiru <99774021+Norikiru@users.noreply.github.com>
2023-04-22 21:42:11 +0800

    MNG: add toggles for guild, everyond, and roles

---
+++ plugins/muteNewGuilds/src/Settings.tsx
@@ -0,0 +1,30 @@
+import { ReactNative as RN } from "@vendetta/metro/common";
+import { Forms } from "@vendetta/ui/components";
+import { useProxy } from "@vendetta/storage";
+import { storage } from "@vendetta/plugin";
+
+const { FormSwitchRow } = Forms;
+
+export default () => {
+    useProxy(storage);
+
+    return (
+        <RN.ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 38 }}>
+            <FormSwitchRow
+                label="Mute Server"
+                value={storage["server"]}
+                onValueChange={(v: boolean) => storage["server"] = v}
+            />
+            <FormSwitchRow
+                label="Suppress @everyone and @here"
+                value={storage["everyone"]}
+                onValueChange={(v: boolean) => storage["everyone"] = v}
+            />
+            <FormSwitchRow
+                label="Suppress All Role @mentons"
+                value={storage["roles"]}
+                onValueChange={(v: boolean) => storage["roles"] = v}
+            />
+        </RN.ScrollView>
+    );
+};
--- plugins/muteNewGuilds/src/index.ts
+++ plugins/muteNewGuilds/src/index.ts
@@ -1,9 +1,15 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
+import { storage } from "@vendetta/plugin";
 import { after } from "@vendetta/patcher";
 import { invites } from "@vendetta/metro/common";
+import Settings from "./Settings";
 
 const { updateGuildNotificationSettings } = findByProps("updateGuildNotificationSettings");
 const UserGuildSettingsStore = findByStoreName("UserGuildSettingsStore");
+
+storage.server ??= false;
+storage.everyone ??= true;
+storage.roles ??= true;
 
 async function patch() {
     const [isMuted, isEveryoneSupressed, isRolesSupressed] = [await UserGuildSettingsStore.isMuted(null), await UserGuildSettingsStore.isSuppressEveryoneEnabled(null), await UserGuildSettingsStore.isSuppressRolesEnabled(null)];
@@ -16,7 +22,7 @@ async function patch() {
             return x.guild.id;
         });
         if (guildId === "@me" || guildId === "null" || guildId == null) return;
-        updateGuildNotificationSettings(guildId, { "muted": true, "suppress_everyone": true, "suppress_roles": true });
+        updateGuildNotificationSettings(guildId, { "muted": storage.server, "suppress_everyone": storage.everyone, "suppress_roles": storage.roles });
     });
 }
 
@@ -26,5 +32,6 @@ export default {
     },
     onUnload: () => {
         patch();
-    }
+    },
+    settings: Settings
 };
